d867d409c90afba6a45b4705540d35b1b7772b2b
gmatheus <gabrielmatheus13@gmail.com>
2022-02-15 22:46:26 -0300

    changes

--- StateMachine.cs
+++ StateMachine.cs
@@ -2,39 +2,40 @@ using Godot;
 using System.Collections.Generic;
 public class StateMachine : Node2D
 {
-    KeyValuePair<Transition, State> TransitionStatePair = new KeyValuePair<Transition, State>();
+  //Transition is unique, State is not 
+  KeyValuePair<Transition, State> TransitionStatePair = new KeyValuePair<Transition, State>();
 
-    class Transitions : List<KeyValuePair<Transition, State>> { } //each state has a transition object assotiated
-    Dictionary<State, Transitions> TransitionMap = new Dictionary<State, Transitions>();
-    private State currentState;
-    public override void _Ready()
+  class Transitions : List<KeyValuePair<Transition, State>> { } //each state has a transition object assotiated
+  Dictionary<State, Transitions> TransitionMap = new Dictionary<State, Transitions>();
+  private State currentState;
+  public override void _Ready()
+  {
+  }
+  private void Update(float delta)
+  {
+    //find the set of transitions for the current state
+    Transitions it = TransitionMap[currentState];
+    //loop through every transition for this state
+    foreach (var transPair in it)
     {
+      if (transPair.Key.ToTransition())
+      {
+        SetState(transPair.Value);
+        break;
+      }
     }
-    private void Update(float delta)
+    if (currentState != null)
     {
-        //find the set of transitions for the current state
-        Transitions it = TransitionMap[currentState];
-        //loop through every transition for this state
-        foreach (var transPair in it)
-        {
-            if (transPair.Key.ToTransition())
-            {
-                SetState(transPair.Value);
-                break;
-            }
-        }
-        if (currentState != null)
-        {
-            currentState.Update(delta);
-        }
+      currentState.Update(delta);
     }
-    private void SetState(State state)
-    {
+  }
+  private void SetState(State state)
+  {
 
-    }
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+  }
+  //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  //  public override void _Process(float delta)
+  //  {
+  //      
+  //  }
 }
