15a78b28c32a1a9cee772036557e91f84cab420e
gmatheus <gabrielmatheus13@gmail.com>
2022-02-21 23:02:00 -0300

    asdasd

--- Scripts/Transitions/Edge_GoHome.cs
+++ Scripts/Transitions/Edge_GoHome.cs
@@ -1,27 +1,31 @@
 using Godot;
 public class Edge_GoHome : Edge
 {
-    Agent_Miner miner;
-    public Edge_GoHome(Agent Agent)
+  Agent_Miner miner;
+  public Edge_GoHome(Agent Agent)
+  {
+    this.miner = (Agent_Miner)Agent;
+  }
+  public override bool ToTransition()
+  {
+    if (miner.FatigueLevel >= miner.FatigueLimit)
     {
-        this.miner = (Agent_Miner)Agent;
-    }
-    public override bool ToTransition()
-    {
-        if (miner.FatigueLevel >= miner.FatigueLimit)
+      if (miner.MoneyInBank >= miner.GoldLimit)
+      {
+        if (miner.GoldCarried <= 0)
         {
+          if (miner.ThirstLevel < miner.ThirstLimit)
+          {
             return true;
+          }
         }
-        if (miner.MoneyInBank >= miner.GoldLimit)
+        else
         {
-            if (miner.GoldCarried <= 0)
-            {
-                if (miner.ThirstLevel < miner.ThirstLimit)
-                {
-                    return true;
-                }
-            }
+          return false;
         }
-        return false;
+      }
+      return true;
     }
+    return false;
+  }
 }
--- Scripts/Transitions/Edge_MineGold.cs
+++ Scripts/Transitions/Edge_MineGold.cs
@@ -1,23 +1,23 @@
 using Godot;
 class Edge_MineGold : Edge
 {
-    Agent_Miner miner;
-    public Edge_MineGold(Agent agent)
+  Agent_Miner miner;
+  public Edge_MineGold(Agent agent)
+  {
+    miner = (Agent_Miner)agent;
+  }
+  public override bool ToTransition()
+  {
+    if (miner.GoldCarried < miner.BagLimit)
     {
-        miner = (Agent_Miner)agent;
-    }
-    public override bool ToTransition()
-    {
-        if (miner.GoldCarried < miner.BagLimit)
+      if (miner.ThirstLevel < miner.ThirstLimit)
+      {
+        if (miner.FatigueLevel == 0)
         {
-            if (miner.ThirstLevel < miner.ThirstLimit)
-            {
-                if (miner.FatigueLevel < miner.FatigueLimit)
-                {
-                    return true;
-                }
-            }
+          return true;
         }
-        return false;
+      }
     }
+    return false;
+  }
 }
